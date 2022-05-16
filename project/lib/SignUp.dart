95c2925a1c322c807df449163d49e922e6e6fa4b
MEHDi <mhdzahedi9936@gmail.com>
2022-05-17 00:16:41 +0430

    m

--- project/lib/SignUp.dart
+++ project/lib/SignUp.dart
@@ -2,13 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
-  SignUp({Key? key}) : super(key: key); ///aaaaaaaaaa
+  SignUp({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => SignUpState();
 }
 
 class SignUpState extends State<SignUp> {
+  bool show = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,35 +24,35 @@ class SignUpState extends State<SignUp> {
                 width: 150,
                 height: 150,
               ),
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               Container(
-                margin: EdgeInsets.fromLTRB(50,10,50,10),
-                child: TextField(                
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextField(
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 15,
-                    fontFamily: 'GoogleSans-Medium', 
+                    fontFamily: 'GoogleSans-Medium',
                     fontWeight: FontWeight.bold,
                   ),
                   // textAlign: TextAlign.center,
                   cursorColor: Colors.black,
-                  decoration: InputDecoration( 
+                  decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.orange,
-                    border: OutlineInputBorder(                      
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(
-                        color: Colors.orange,
-                      )
-                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(
+                          color: Colors.orange,
+                        )),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange)
-                    ),
+                        borderSide: BorderSide(color: Colors.orange)),
                     hintText: 'Email',
                     hintStyle: TextStyle(
                       color: Colors.black,
                       fontSize: 15,
-                      fontFamily: 'GoogleSans-Medium', 
+                      fontFamily: 'GoogleSans-Medium',
                       fontWeight: FontWeight.bold,
                     ),
                     contentPadding: EdgeInsets.all(25),
@@ -59,33 +60,31 @@ class SignUpState extends State<SignUp> {
                 ),
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(50,10,50,10),
-                child: TextField(                
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextField(
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 15,
-                    fontFamily: 'GoogleSans-Medium', 
+                    fontFamily: 'GoogleSans-Medium',
                     fontWeight: FontWeight.bold,
                   ),
                   // textAlign: TextAlign.center,
                   cursorColor: Colors.black,
-                  decoration: InputDecoration( 
+                  decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.orange,
-                    border: OutlineInputBorder(                      
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(
-                        color: Colors.orange,
-                      )
-                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(
+                          color: Colors.orange,
+                        )),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange)
-                    ),
+                        borderSide: BorderSide(color: Colors.orange)),
                     hintText: 'Username',
                     hintStyle: TextStyle(
                       color: Colors.black,
                       fontSize: 15,
-                      fontFamily: 'GoogleSans-Medium', 
+                      fontFamily: 'GoogleSans-Medium',
                       fontWeight: FontWeight.bold,
                     ),
                     contentPadding: EdgeInsets.all(25),
@@ -93,41 +92,38 @@ class SignUpState extends State<SignUp> {
                 ),
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(50, 10,50,0),
-                child: TextField(                
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+                child: TextField(
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 15,
-                    fontFamily: 'GoogleSans-Medium', 
+                    fontFamily: 'GoogleSans-Medium',
                     fontWeight: FontWeight.bold,
                   ),
                   // textAlign: TextAlign.center,
                   cursorColor: Colors.black,
-                  decoration: InputDecoration( 
+                  decoration: InputDecoration(
                     suffixIcon: Container(
                       margin: EdgeInsets.only(right: 20),
-                      child: Icon(
-                        Icons.visibility_off,
-                        color: Colors.black,
-                        
+                      child: IconButton(
+                        onPressed: () {show = !show;},
+                        icon: Icon(show ? Icons.visibility_off : Icons.visibility),
                       ),
                     ),
                     filled: true,
                     fillColor: Colors.orange,
-                    border: OutlineInputBorder(                      
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(
-                        color: Colors.orange,
-                      )
-                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(
+                          color: Colors.orange,
+                        )),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange)
-                    ),
+                        borderSide: BorderSide(color: Colors.orange)),
                     hintText: 'Password',
                     hintStyle: TextStyle(
                       color: Colors.black,
                       fontSize: 15,
-                      fontFamily: 'GoogleSans-Medium', 
+                      fontFamily: 'GoogleSans-Medium',
                       fontWeight: FontWeight.bold,
                     ),
                     contentPadding: EdgeInsets.all(25),
@@ -136,7 +132,6 @@ class SignUpState extends State<SignUp> {
               ),
             ],
           ),
-        )
-    );
+        ));
   }
 }

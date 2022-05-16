47b9a4603bb55699b37b13f91f6f9dd7f6a3ff65
MEHDi <mhdzahedi9936@gmail.com>
2022-05-17 00:07:01 +0430

    3

--- project/lib/SignUp.dart
+++ project/lib/SignUp.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
-  SignUp({Key? key}) : super(key: key);
+  SignUp({Key? key}) : super(key: key); ///aaaaaaaaaa
 
   @override
   State<StatefulWidget> createState() => SignUpState();

12e855256138f6717c00acdc72a29d18ad802e82
MEHDi <mhdzahedi9936@gmail.com>
2022-05-19 01:54:39 +0430

    01:54 AM 19 May

---
+++ project/lib/PostModel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class PostModel {
+  late String publisherName; //** Community Name */
+  late String userName; //** User Name */
+  late DateTime gregorianDate; //** Gregorian date */
+  late DateTime solarDate; //** تاریخ شمسی */
+  late String text; //** text */
+  late int numLikes;
+  late int numDisLike;
+  late int numComments;
+}

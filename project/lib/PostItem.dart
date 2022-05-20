6136eecfe6d8532cf578d462a3c16b03c34d095d
MEHDi <mhdzahedi9936@gmail.com>
2022-05-20 17:23:07 +0430

    05:22 PM 20 May

---
+++ project/lib/PostItem.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project/PostModel.dart';
+
+class PostItem extends StatelessWidget {
+  PostItem(this._postModel);
+  PostModel _postModel;
+  int likeState = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity, //* Set to Page */
+      margin: EdgeInsets.all(30),
+      height: 150,
+      decoration: BoxDecoration(
+        //** Beautiful :) */
+        color: Colors.black,
+        border: Border.all(
+          color: Colors.orange,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                //** Profile Image */
+                radius: 30.0,
+                backgroundColor: Colors.orange,
+                //  child: Image.asset(), //** incompelete Profile */ //* 14 minuts 41////
+              ),
+              Column(
+                //** Publisher and UserName */
+                children: [
+                  Text(
+                    _postModel.publisherName, //** Publisher Name */
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'GoogleSans-Medium',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    //** UserName and DateTime */
+                    _postModel.userName +
+                        '.' +
+                        DateTime.now()
+                            .difference(_postModel.gregorianDate)
+                            .inHours
+                            .toString(),
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'GoogleSans-Medium',
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            //** Text of post */
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 20),
+            child: Text(
+              _postModel.text,
+              style: TextStyle(
+                color: Colors.orange,
+                fontFamily: 'GoogleSans-Medium',
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Row( //** Optional Keys */
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                //** Like , numLikes , DisLike */
+                color: Colors.black,
+                margin: EdgeInsets.only(left: 20),
+                height: 30,
+                child: Row(
+                  //** Icons and Text */
+                  children: [
+                    IconButton( //** Like Icon */
+                      icon: Icon(
+                        Icons.thumb_up_alt_outlined,
+                        color: likeState == 1 ? Colors.orange : Colors.black,
+                      ),
+                      onPressed: () {
+                        likeState == 1
+                            ? _postModel.numLikes--
+                            : _postModel.numLikes++;
+                      },
+                    ),
+                    Text(
+                      (_postModel.numLikes - _postModel.numDisLikes).toString(),
+                      style: TextStyle(
+                        color: likeState == 1 ? Colors.orange : Colors.grey,
+                        
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- project/lib/PostModel.dart
+++ project/lib/PostModel.dart
@@ -9,6 +9,7 @@ class PostModel {
   set publisherName(String publisherName) {
     _publisherName = publisherName;
   } //** Community Name */
+
   late String _userName;
 
   String get userName => _userName;
@@ -16,6 +17,7 @@ class PostModel {
   set userName(String userName) {
     _userName = userName;
   } //** User Name */
+
   late DateTime _gregorianDate;
 
   DateTime get gregorianDate => _gregorianDate;
@@ -23,6 +25,7 @@ class PostModel {
   set gregorianDate(DateTime gregorianDate) {
     _gregorianDate = gregorianDate;
   } //** Gregorian date */
+
   late DateTime _solarDate;
 
   DateTime get solarDate => _solarDate;
@@ -30,6 +33,7 @@ class PostModel {
   set solarDate(DateTime solarDate) {
     _solarDate = solarDate;
   } //** تاریخ شمسی */
+
   late String _text;
 
   String get text => _text;
@@ -37,6 +41,7 @@ class PostModel {
   set text(String text) {
     _text = text;
   } //** text */
+
   late int _numLikes;
 
   int get numLikes => _numLikes;
@@ -44,6 +49,7 @@ class PostModel {
   set numLikes(int numLikes) {
     _numLikes = numLikes;
   }
+
   late int _numDisLikes;
 
   int get numDisLikes => _numDisLikes;
@@ -51,6 +57,7 @@ class PostModel {
   set numDisLikes(int numDisLikes) {
     _numDisLikes = numDisLikes;
   }
+
   late int _numComments;
 
   int get numComments => _numComments;
@@ -58,20 +65,28 @@ class PostModel {
   set numComments(int numComments) {
     _numComments = numComments;
   }
-  List<PostModel> _comments = []; //** incomplete */
 
-  List<PostModel> get comments => _comments; //** incomplete */
+  List<PostModel> _comments = [];
 
-  set comments(List<PostModel> comments) { //** incomplete */
+  List<PostModel> get comments => _comments;
+
+  set comments(List<PostModel> comments) {
     _comments = comments;
   }
 
-  // void addComment() { //** incomplete */
- 
-  // }
-  
-  //** Constructor */
-  PostModel(this._publisherName,this._userName,this._gregorianDate,
-    this._solarDate,this._text,this._numLikes,this._numDisLikes,this._numComments,this._comments);
+  void addComment(PostModel comment) {
+    _comments.add(comment);
+  }
 
+  //** Constructor */
+  PostModel(
+      this._publisherName,
+      this._userName,
+      this._gregorianDate,
+      this._solarDate,
+      this._text,
+      this._numLikes,
+      this._numDisLikes,
+      this._numComments,
+      this._comments);
 }
